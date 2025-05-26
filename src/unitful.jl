4319b4327127df640447d35ff505960fa50bdeb5
WooKyoung Noh <wookay.noh@gmail.com>
2025-05-26 15:25:18 +0900

    test internal functions

--- src/unitful.jl
+++ src/unitful.jl
@@ -1,10 +1,30 @@
 # module TimeUnits
 
 using Unitful: Unitful
-using .Unitful: Quantity, unit
-using .Unitful: s, ms, μs, ns, ps, fs, as
+using .Unitful: Quantity, FreeUnits, unit
+using .Unitful: d, hr, minute, s, ms, μs, ns, ps, fs, as
 
 const fractional_si_units = (s, ms, μs, ns, ps, fs, as)
+
+unit_base(::typeof(hr))     = 24
+unit_base(::typeof(minute)) = 60
+unit_base(::typeof(s))      = 60
+unit_base(::typeof(ms))     = 1000
+unit_base(::typeof(μs))     = 1000
+unit_base(::typeof(ns))     = 1000
+unit_base(::typeof(ps))     = 1000
+unit_base(::typeof(fs))     = 1000
+unit_base(::typeof(as))     = 1000
+
+parent_unit(::typeof(hr))     = d
+parent_unit(::typeof(minute)) = hr
+parent_unit(::typeof(s))      = minute
+parent_unit(::typeof(ms))     = s
+parent_unit(::typeof(μs))     = ms
+parent_unit(::typeof(ns))     = μs
+parent_unit(::typeof(ps))     = ns
+parent_unit(::typeof(fs))     = ps
+parent_unit(::typeof(as))     = fs
 
 # original code from julia/base/intfuncs.jl
 # function _base(base::Integer, x::Integer, pad::Int, neg::Bool)
@@ -12,8 +32,15 @@ function base_thousand(x::Integer, neg::Bool)
     b = 1000
     (x >= 0) | (b < 0) || throw(DomainError(x, "For negative `x`, `base` must be negative."))
     pad = 0
-    n = neg + ndigits(x, base=b, pad=pad)
-    i = n
+
+    n10 = ndigits(x, base=10, pad=pad)
+    remainder = 3 - rem(n10, 3)
+    if remainder != 3
+        x = x * 10 ^ remainder
+    end
+
+    n = ndigits(x, base=b, pad=pad)
+    i = neg + n
     unit_nums = []
     @inbounds while i > neg
         if b > 0
@@ -29,18 +56,47 @@ function base_thousand(x::Integer, neg::Bool)
     unit_nums
 end
 
-function canonical_fractional_part(x::Int)::Vector{Quantity{Int}}
+function canonical_fractional_part(x::Int, distance::Int)::Vector{Quantity{Int}}
     neg = signbit(x)
     periods = Vector{Quantity{Int}}()
     for (i, num) in base_thousand(neg ? -x : x, neg)
-        unit = fractional_si_units[1 + i - neg]
-        push!(periods, (num)unit)
+        U = fractional_si_units[distance + 1 + i - neg]
+        push!(periods, (num)U)
     end
     periods
 end
 
-# s
-function canonical_floating_parts(val::Float64)::Vector{Quantity{Int}}
+function canonical_compound_periods(compound_periods::Vector{Quantity{Int}})::Vector{Quantity{Int}}
+    one_more_time = false
+    periods = Vector{Quantity{Int}}()
+    for q in compound_periods
+        x = q.val
+        iszero(x) && continue
+        U = unit(q)
+        P = parent_unit(U)
+        base = unit_base(U)
+        if x >= base
+            quotient  = div(x, base)
+            remainder = rem(x, base)
+            push!(periods, (quotient)P)
+            if !iszero(remainder)
+                push!(periods, (remainder)U)
+            end
+            if quotient >= unit_base(P)
+                one_more_time = true
+            end
+        else
+            push!(periods, q)
+        end
+    end
+    if one_more_time
+        canonical_compound_periods(periods)
+    else
+        periods
+    end
+end
+
+function canonical_floating_parts(val::Float64, U::FreeUnits)::Vector{Quantity{Int}}
     integral = Int(div(val, 1))
     neg = signbit(val)
     n = neg + ndigits(integral, base=10)
@@ -48,27 +104,28 @@ function canonical_floating_parts(val::Float64)::Vector{Quantity{Int}}
     p = length(str) - n - 1
     fractional = val - integral
     x = round(Int, fractional * ^(10, p))
-    periods = Vector{Quantity{Int}}()
+    compound_periods = Vector{Quantity{Int}}()
     if !neg && integral > 0
-        push!(periods, (integral)s)
+        push!(compound_periods, (integral)U)
     elseif neg && integral < 0
-        push!(periods, (integral)s)
+        push!(compound_periods, (integral)U)
     end
     if !iszero(x)
-        part = canonical_fractional_part(x)
-        append!(periods, part)
+        distance = ndigits(U(1s).val, base=1000) - 1
+        part = canonical_fractional_part(x, distance)
+        append!(compound_periods, part)
     end
-    periods
+    canonical_compound_periods(compound_periods)
 end
 
-function canonical_floating_parts(r::Rational{Int})::Vector{Quantity{Int}}
-    canonical_floating_parts(Float64(r))
+function canonical_floating_parts(r::Rational{Int}, U::FreeUnits)::Vector{Quantity{Int}}
+    canonical_floating_parts(Float64(r), U)
 end
 
 struct Compound{T}
     periods::Vector{Quantity{T}}
     function Compound(q::Quantity{Float64})
-        periods = canonical_floating_parts(q)
+        periods = canonical_floating_parts(q.val, unit(q))
         if isempty(periods)
             new{Int}([0s])
         else
@@ -79,43 +136,17 @@ struct Compound{T}
         if isempty(qs)
             new{Int}([])
         else
-            if any(q -> unit(q) === as, qs)
-                new{Int}(collect(qs))
+            compound_periods = Vector{Quantity{Int}}(collect(qs))
+            periods = canonical_compound_periods(compound_periods)
+            if length(qs) == 1 && isempty(periods)
+                new{Int}([0s])
             else
-                val = sum(q -> s(q).val, qs)
-                if val isa Int
-                    new{Int}([(val)s])
-                else
-                    periods = canonical_floating_parts(val)
-                    new{Int}(periods)
-                end
+                new{Int}(periods)
             end
         end
     end
     function Compound{T}(periods::Vector{Quantity{T}}) where T
         new{T}(periods)
-    end
-end
-
-function shift_down(compound::Vector{Quantity{Int}}, down::Int)::Vector{Quantity{Int}}
-    periods = Vector{Quantity{Int}}()
-    for q in compound
-        u = unit(q)
-        unit_index = findfirst(x -> x === u, fractional_si_units)
-        down_u = fractional_si_units[unit_index + down]
-        push!(periods, (q.val)down_u)
-    end
-    return periods
-end
-
-function canonical_floating_parts(q::Quantity{Float64})::Vector{Quantity{Int}}
-    u = unit(q)
-    if u === s
-        canonical_floating_parts(q.val)
-    else
-        unit_index = findfirst(x -> x === u, fractional_si_units)
-        periods = canonical_floating_parts(q.val)
-        shift_down(periods, unit_index - 1)
     end
 end
 
--- test/timeunits/Compound.jl
+++ test/timeunits/Compound.jl
@@ -4,7 +4,31 @@ using Test
 using TimeUnits # Compound
 using Unitful: minute, s, ms, μs, ns, ps, fs, as
 
+using TimeUnits: base_thousand, canonical_fractional_part, canonical_floating_parts
+
+@test base_thousand(3,   false) == [(1, 300)]
+@test base_thousand(37,  false) == [(1, 370)]
+@test base_thousand(370, false) == [(1, 370)]
+
+@test base_thousand(545_454_545_47, false) == [(1, 545), (2, 454), (3, 545), (4, 470)]
+@test base_thousand(545_454_545_47, true)  == [(2, -545), (3, -454), (4, -545), (5, -470)]
+
+@test canonical_fractional_part(123, 0) == [123ms]
+@test canonical_fractional_part(123, 1) == [123μs]
+
+@test canonical_floating_parts(0.123, s)               == [123ms]
+@test canonical_floating_parts(0.123_456, s)           == [123ms, 456μs]
+@test canonical_floating_parts(123.0, ms)              == [123ms]
+@test canonical_floating_parts(123.456, ms)            == [123ms, 456μs]
+@test canonical_floating_parts(123.456_789, ms)        == [123ms, 456μs, 789ns]
+
+@test canonical_floating_parts(226_279.545_454_545, ms)    == [3minute, 46s, 279ms, 545μs, 454ns, 545ps]
+@test canonical_floating_parts(226_279.545_454_545_47, ms) == [3minute, 46s, 279ms, 545μs, 454ns, 545ps, 470fs]
+
 @test 1fs == 1000as == (1/1000)ps
+
+@test Compound()                       == Compound()
+
 @test Compound(0.0s)                   == Compound(0s)
 @test Compound(3.141_592s)             == Compound(3s, 141ms, 592μs)
 @test Compound(3.141_592_653_589_793s) == Compound(3s, 141ms, 592μs, 653ns, 589ps, 793fs)
@@ -13,17 +37,16 @@ using Unitful: minute, s, ms, μs, ns, ps, fs, as
 
 @test Compound(60s)                    == Compound(1minute)
 @test Compound(63s)                    == Compound(1minute, 3s)
-@test Compound(63s).periods            == [63s]
-@test Compound(1minute, 3s).periods    == [63s]
+@test Compound(63s).periods            == [1minute, 3s]
+@test Compound(1minute, 3s).periods    == [1minute, 3s]
 @test Compound(63.141s)                == Compound(1minute, 3s, 141ms)
 
 @test Compound(141.592ms)              == Compound(141ms, 592μs)
 @test Compound(141.592ms).periods      == [141ms, 592μs]
+@test Compound(0as)                    == Compound(0as)
 
-@test Compound()                       == Compound()
-
-# special for as
-@test Compound(0as)                                      == Compound(0as)
 @test Compound(620ms, 004μs, 545ns, 454ps, 545fs, 455as) == Compound(620ms, 004μs, 545ns, 454ps, 545fs, 455as)
+@test Compound(226_279.545_454_545_47ms)                 == Compound(3minute, 46s, 279ms, 545μs, 454ns, 545ps, 470fs)
+@test Compound(620.454_545_454_545_5ms) == Compound(620ms, 454μs, 545ns, 454ps, 545fs, 500as)
 
 end # module test_timeunits_Compound
